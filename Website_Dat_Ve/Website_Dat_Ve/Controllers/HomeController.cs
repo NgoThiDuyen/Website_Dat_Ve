6405f6cf4d8ffe07f0f8b41c0206be0c3399c1c4
NgoThiDuyen <duyen.nt207669@sis.hust.edu.vn>
2023-06-26 01:44:26 +0700

    trangchu

--- Website_Dat_Ve/Website_Dat_Ve/Controllers/HomeController.cs
+++ Website_Dat_Ve/Website_Dat_Ve/Controllers/HomeController.cs
@@ -1,16 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Web_Dat_Ve_Xem_Phim.Models;
 
 namespace Website_Dat_Ve.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private readonly HttpClient _httpClient;
+
+        public HomeController()
         {
-            return View();
+            _httpClient = new HttpClient();
+            // Cấu hình các thông tin liên quan đến API như địa chỉ base URL, token xác thực, ...
+            _httpClient.BaseAddress = new Uri("http://192.168.1.15:8086/");
+            _httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer YourAccessToken");
+        }
+        public async Task<ActionResult> Index()
+        {
+            string apiUrl = _httpClient.BaseAddress + "api/Movie/GetMovieByCategory";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var movieResponse = await response.Content.ReadAsAsync<MovieResponse>();
+                    return View(movieResponse.Data);
+                }
+                else
+                {
+                    // Xử lý khi gọi API không thành công
+                    return View(new List<MovieVM>());
+                }
+            }
         }
 
         public ActionResult About()
---
+++ Website_Dat_Ve/Website_Dat_Ve/Models/MovieVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web_Dat_Ve_Xem_Phim.Models
+{
+    public class MovieResponse
+    {
+        public string Code { get; set; }
+        public string Des { get; set; }
+        public string Message { get; set; }
+        public List<MovieVM> Data { get; set; }
+    }
+    public class MovieVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Cast { get; set; }
+        public int MovieType { get; set; }
+        public int Time { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public int NumberBooking { get; set; }
+        public int NumberView { get; set; }
+    }
+}
